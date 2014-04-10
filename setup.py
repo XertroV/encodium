a37bff33e391c66ba2ae0dfa5ebbe8810e8bf618
Kitten Tofu <kitten@eudemonia.io>
2014-04-11 09:27:11 +1000

    moved everything over from spore, removed rlp serialization

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tetris',
+      version='0.0.1',
+      description='Yet another pure python serialization module',
+      author='Kitten Tofu',
+      author_email='kitten@eudemonia.io',
+      url='http://eudemonia.io/tetris/',
+      packages=['tetirs'],
+     )

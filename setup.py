977a9e3b70a782b0106c6aaeb7a346db3e3a0945
Kitten Tofu <kitten@eudemonia.io>
2014-04-20 13:28:55 +1000

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='encodium',
-      version='0.0.4',
+      version='0.0.5',
       description='Yet another pure python serialization module',
       author='Kitten Tofu',
       author_email='kitten@eudemonia.io',

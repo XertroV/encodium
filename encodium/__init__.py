f210d9c14062bb16985955e5a0ec77c8761e62e1
Kitten Tofu <kitten@eudemonia.io>
2014-06-29 17:09:34 -0700

    added Integer non_negative, String max_length, and optional arguments and type checking

--- encodium/__init__.py
+++ encodium/__init__.py
@@ -73,17 +73,6 @@ Type checking is also included automatically::
 
     john = Person(age="this is not an integer")
 
-Constraints can be implemented by defining ``check_attribute()`` on the type's
-``Definition`` class, as thus::
-
-    from encodium import Encodium, ValidationError
-
-    class Integer(int):
-        class Definition(Encodium.Definition):
-            def check_attribute(self, value):
-                if self.non_negative and value < 0:
-                    raise ValidationError('cannot be negative')
-
 More complex validation can be done by defining ``check()`` on the object.
 
 A useful paradigm when using encodium is to use the following invariant:
@@ -108,6 +97,20 @@ Here's an example to illustrates this::
             if self.hash != expected_hash:
                 raise ValidationError('has an invalid hash')
 
+Custom Constraints
+------------------
+
+Constraints can be implemented by defining ``check_value()`` on the type's
+``Definition`` class, as thus::
+
+    from encodium import Encodium, ValidationError
+
+    class Integer(int):
+        class Definition(Encodium.Definition):
+            def check_value(self, value):
+                if self.non_negative and value < 0:
+                    raise ValidationError('cannot be negative')
+
 
 Recursive Definitions
 ---------------------
@@ -127,46 +130,22 @@ instead of ``ClassName.Definition(...)``, as thus::
 
 '''
 
-import types
 import sys
 
-# For now, None is always serialized as b''.
-# TODO: introduce hook to make this more flexible.
-# NOTE: all serialization must be at least one byte long so it can be
-# differentiated from None.
-
-# TODO: change all variables of type "field" to name "field"
-#       rename all other variables called "field"
-# TODO: Deal with None serialization properly.
 
 class ValidationError(Exception):
+    ''' Raise in the case of a validation error.
+    Always in a form that can be appended to the name of a field.
+    e.g. _____ "cannot be greater or equal to 10"
+    '''
     pass
 
 
-def _encodium_get_locals(func):
-    ret = None
+class Field:
+    ''' This class included for backwards compatibility. '''
 
-    def tracer(frame, event, arg):
-        nonlocal ret
-        if event == 'return':
-            ret = frame.f_locals.copy()
-
-    # tracer is activated on next call, return or exception
-    old_tracer = sys.getprofile()
-    sys.setprofile(tracer)
-    try:
-        # trace the function call
-        func()
-    finally:
-        # disable tracer and replace with old one
-        sys.setprofile(old_tracer)
-    return ret
-
-
-class Field(object):
-    _order = 0
-
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
+        ''' Displays an error message to stderr and raises an exception. '''
         msg = "Use of encodium.Field is Deprecated.\n"
         msg += "This change will break backwards compatibility\n"
         msg += "For a quickfix, change:\n"
@@ -175,281 +154,126 @@ class Field(object):
         msg += "    from encodium.deprecated import ___\n"
         sys.stderr.write(msg)
         raise Exception("Upgrade Encodium")
-        
-        # Give this instance a number, to restore ordering later.
-        self._order = Field._order
-        Field._order += 1
-
-        if not hasattr(self, 'type'):
-
-            class FieldInstance(object):
-
-                def __init__(inner_self, *args, **kwargs):
-
-                    # Add all the kwargs:
-                    for key, value in kwargs.items():
-                        setattr(inner_self, key, value)
-
-                    # Add None for the fields that weren't set:
-                    for key, field in self.get_fields():
-                        if not hasattr(inner_self, key):
-                            setattr(inner_self, key, None)
-
-                    if hasattr(inner_self, 'init'):
-                        inner_self.init()
-
-                def serialize(inner_self):
-                    return self.serialize(inner_self)
-
-                def __eq__(inner_self, other):
-                    if inner_self.__class__.__name__ != other.__class__.__name__:
-                        return False
-                    fields = self.get_fields()
-                    for name, field in fields:
-                        if getattr(inner_self, name) != getattr(other, name):
-                            return False
-                    return True
-
-                def __setattr__(inner_self, key, value):
-                    nonlocal self
-                    fields = dict(self.get_fields())
-                    if key in fields:
-                        field = fields[key]
-                        if value is None:
-                            if callable(field.default):
-                                value = field.default()
-                            else:
-                                value = field.default
-                        try:
-                            if value is None:
-                                field.check_optional(value)
-                            else:
-                                field.check(value)
-                                field.check_type(value)
-                        except ValidationError as e:
-                            # Prepend the key to the exception message
-                            e.args = (key + " " + e.args[0],) + e.args[1:]
-                            raise
-                    inner_self.__dict__[key] = value
 
 
-            self.type = type(self.__class__.__name__ + 'Instance',
-                             (FieldInstance,),
-                             dict(self.__class__.__dict__))
+class EncodiumMeta(type):
+    def __init__(cls, name, bases, dict):
+        super().__init__(name, bases, dict)
+        # If this is not the base class, create some useful variables.
+        if name != 'Encodium':
 
-        # Add the REAL make (not a class function, instance function)
+            # Used for type checking.
+            if not hasattr(cls.Definition, '_encodium_type'):
+                cls.Definition._encodium_type = cls
 
-        def make(inner_self, _data=None, *args, **kwargs):
-            if _data is not None:
-                return inner_self.deserialize(_data)
-            ret = self.type(*args, **kwargs)
-            self.check(ret)
-            self.check_optional(ret)
-            self.check_type(ret)
-            return ret
+            # Used to easily access the fields (can be overriden)
+            if not hasattr(cls.Definition, '_encodium_fields'):
+                cls._encodium_fields = []
+                for key, value in dict.items():
+                    if isinstance(value, Encodium.Definition):
+                        cls._encodium_fields.append(key)
 
-        self.make = types.MethodType(make, self)
 
-        options = _encodium_get_locals(self.__class__.default_options)
-        self.default = None
-        self.optional = None
-        for key, value in options.items():
-            setattr(self, key, value)
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+class Encodium(metaclass=EncodiumMeta):
+    ''' This is the base class for all Encodium objects.
+    '''
 
-    def default_options():
+    class Definition:
+        optional = False
+        default = None
+
+        def __init__(self, **kwargs):
+            # Copy across kwargs.
+            for key, value in kwargs.items():
+                self.__dict__[key] = value
+
+        def check_type(self, value):
+            if not self.optional and value is None:
+                raise ValidationError('cannot not be None')
+            expected = self._encodium_type
+            actual = value.__class__
+            if not issubclass(actual, expected):
+                message = 'is supposed to be type ' + str(expected)
+                message += ', but was set to something of type ' + str(actual) + '.'
+                raise ValidationError(message)
+
+        def check_attribute(self, value):
+            pass
+
+
+    def __init__(self, *args, **kwargs):
+        for field in self._encodium_fields:
+            if field not in kwargs:
+                definition = self.__class__.__dict__[field]
+                kwargs[field] = definition.default
+
+        self.change(**kwargs)
+
+    def change(self, **kwargs):
+        changed_attributes = {}
+        for field, value in kwargs.items():
+            if field not in self._encodium_fields:
+                # TODO: decide how to handle this case.
+                sys.stderr.write("Warning: Argument " + field +
+                                 " provided but isn't a field for" +
+                                 " Encodium type " + self.__class__.__name__)
+            else:
+                definition = self.__class__.__dict__[field]
+
+                try:
+                    definition.check_type(value)
+                    if value is not None:
+                        definition.check_value(value)
+                    changed_attributes[field] = value
+                except ValidationError as e:
+                    # Prepend the name of the field to the exception message
+                    e.args = (field + " " + e.args[0],) + e.args[1:]
+                    raise
+
+        backup = {}
+        for field, value in changed_attributes.items():
+            try:
+                backup[field] = self.__dict__[field]
+            except KeyError:
+                backup[field] = None
+            self.__dict__[field] = value
+
+        try:
+            self.check(changed_attributes.keys())
+        except ValidationError as e:
+            # Restore the backup before re-raising.
+            for field, value in backup:
+                self.__dict__[field] = value
+            raise
+
+    def check(self, changed_attributes):
         pass
 
-    def check(self, instance):
-        pass
 
-    def check_optional(self, value):
-        if not self.optional and value is None:
-            raise ValidationError("cannot be None")
+class Integer(Encodium):
+    class Definition(Encodium.Definition):
+        _encodium_type = int
+        non_negative = False
 
-    def check_type(self, value):
-        if value.__class__.__name__ != self.type.__name__:
-            instance_type = value.__class__.__name__
-            expected_type = self.type.__name__
-            raise ValidationError("is of type " + instance_type + ", expected " + expected_type)
-
-    def get_fields(self):
-        # Default serialize is to go through each of the fields.
-        if not hasattr(self.__class__, 'fields'):
-            raise NotImplementedError(self.__class__.__name__ + " has no fields")
-        fields = _encodium_get_locals(self.__class__.fields)
-        ordered_fields = []
-        for key, value in fields.items():
-            if isinstance(value, Field):
-                ordered_fields.append((value._order, key, value))
-        ordered_fields.sort()
-        return [(key, value) for _, key, value in ordered_fields]
-
-    def serialize(self, value):
-        def encode_length(length):
-            encoded_length = length.to_bytes((length.bit_length() + 7) >> 3, 'big')
-            if length >= 0xfa:
-                encoded_length_length = len(encoded_length)
-                if encoded_length_length > 6:
-                    raise ValidationError("length too big")
-                encoded_length = bytes([encoded_length_length + 0xf9]) + encoded_length
-            return encoded_length
-
-        array = [b'\x01']
-        for key, field in self.get_fields():
-            attr = getattr(value, key)
-            if attr is None:
-                array.append(b'\x00')
-            else:
-                data = field.serialize(attr)
-                array.append(encode_length(len(data)))
-                array.append(data)
-        return b''.join(array)
-
-    def deserialize(self, data):
-        def decode_length(data, index=0):
-            length = int.from_bytes(data[index:index + 1], 'big')
-            length_length = 1
-            if length >= 0xfa:
-                length_length = 1 + (length - 0xf9)
-                length = int.from_bytes(data[index + 1:index + 1 + (length - 0xf9)], 'big')
-            # TODO: validation on decoded length
-            return length, length_length
-
-        i = 1
-        array = []
-        while i < len(data):
-            length, length_length = decode_length(data, i)
-            array.append(data[i + length_length:i + length_length + length])
-            i += length_length + length
-        kwargs = {}
-        for (key, field), item in zip(self.get_fields(), array):
-            if item is b'':
-                kwargs[key] = None
-            else:
-                kwargs[key] = field.deserialize(item)
-        return self.make(**kwargs)
-
-    @classmethod
-    def make(cls, *args, **kwargs):
-        return cls().make(*args, **kwargs)
+        def check_value(self, value):
+            if self.non_negative and value < 0:
+                raise ValidationError("must not be negative")
+            pass
 
 
-class String(Field):
-    type = str
-
-    def default_options():
+class String(Encodium):
+    class Definition(Encodium.Definition):
+        _encodium_type = str
         max_length = None
 
-    def check(self, value):
-        if self.max_length is not None and len(value) > self.max_length:
-            raise ValidationError("too long")
-
-    def serialize(self, s):
-        # Pity about this, no easy way to store the empty string. :(
-        return b'\x01' + s.encode('utf-8')
-
-    def deserialize(self, data):
-        return data[1:].decode('utf-8')
+        def check_value(self, value):
+            if self.max_length is not None and len(value) >= self.max_length:
+                raise ValidationError("was set to a string of length " + str(len(value)) + " but cannot be longer than " + str(self.max_length))
 
 
-class Integer(Field):
-    type = int
+class Boolean(Encodium):
+    class Definition(Encodium.Definition):
+        _encodium_type = bool
 
-    def default_options():
-        signed = True
-
-    def serialize(self, i):
-        bits = i.bit_length()
-        if self.signed and i > 0 and bits % 8 == 0:
-            bits += 1
-        return i.to_bytes(max((bits + 7) >> 3, 1), 'big', signed=self.signed)
-
-    def deserialize(self, data):
-        return int.from_bytes(data, 'big', signed=self.signed)
-
-
-class List(Field):
-    type = list
-
-    def __init__(self, inner_field, *args, **kwargs):
-        self.inner_field = inner_field
-        super().__init__(*args, **kwargs)
-
-    def check_type(constraints, instances):
-        if instances.__class__ != list:
-            instances_type = instances.__class__.__name__
-            raise ValidationError("is of type" + instances_type + ", expected list")
-        for instance in instances:
-            try:
-                constraints.inner_field.check_type(instance)
-            except ValidationError as e:
-                e.args = ("inner element " + e.args[0],) + e.args[1:]
-                raise
-
-    def check(constraints, instances):
-        for instance in instances:
-            try:
-                constraints.inner_field.check(instance)
-            except ValidationError as e:
-                e.args = ("inner element " + e.args[0],) + e.args[1:]
-                raise
-
-    def serialize(self, l):
-        def encode_length(length):
-            encoded_length = length.to_bytes((length.bit_length() + 7) >> 3, 'big')
-            if length >= 0xfa:
-                encoded_length_length = len(encoded_length)
-                if encoded_length_length > 6:
-                    raise ValidationError("length too big")
-                encoded_length = bytes([encoded_length_length + 0xf9]) + encoded_length
-            return encoded_length
-
-        array = [b'\x01']
-        for attr in l:
-            if attr is None:
-                array.append(b'\x00')
-            else:
-                data = self.inner_field.serialize(attr)
-                array.append(encode_length(len(data)))
-                array.append(data)
-        return b''.join(array)
-
-    def deserialize(self, data):
-        def decode_length(data, index=0):
-            length = int.from_bytes(data[index:index + 1], 'big')
-            length_length = 1
-            if length >= 0xfa:
-                length_length = 1 + (length - 0xf9)
-                length = int.from_bytes(data[index + 1:index + 1 + (length - 0xf9)], 'big')
-            # TODO: validation on decoded length
-            return length, length_length
-
-        i = 1
-        array = []
-        while i < len(data):
-            length, length_length = decode_length(data, i)
-            array.append(self.inner_field.deserialize(data[i + length_length:i + length_length + length]))
-            i += length_length + length
-        return array
-
-
-class Boolean(Field):
-    type = bool
-
-    def serialize(self, b):
-        return (b'\x01' if b else b'\x00')
-
-    def deserialize(self, data):
-        return data == b'\x01'
-
-
-class Bytes(Field):
-    type = bytes
-
-    def serialize(self, b):
-        return b'\x01' + b
-
-    def deserialize(self, data):
-        return data[1:]
+        def check_value(self, value):
+            pass
--- tests/tests.py
+++ tests/tests.py
@@ -1,62 +1,74 @@
-from encodium import *
+from encodium import Encodium, Integer, String, Boolean, ValidationError
 import unittest
 
-class TestSerializable(unittest.TestCase):
 
-    def test_person(self):
+class Person(Encodium):
+    age = Integer.Definition(non_negative=True)
+    name = String.Definition(max_length=50)
+    diabetic = Boolean.Definition(default=True)
 
-        class Person(Field):
 
-            def fields():
-                name     = String(max_length=20)
-                age      = Integer(default=0)
-                children = List(Person(), default=[])
-                is_dead  = Boolean(default=False)
-                hat      = String(optional=True)
-                mother   = Person(allow_hat=False, optional=True)
-                privkey  = Bytes(default=b'')
+class TestTypeChecker(unittest.TestCase):
+    def test_encodium_type(self):
+        self.assertEqual(Person.Definition._encodium_type, Person)
 
-            def default_options():
-                allow_hat = True
+    def test_raises_error_on_invalid_type(self):
+        self.assertRaises(ValidationError, Person, age='not a number!', name='correct', diabetic=True)
+        self.assertRaises(ValidationError, Person, age=10, name='correct', diabetic=['Not a bool'])
+        self.assertRaises(ValidationError, Person, age=10, name=False, diabetic=True)
 
-            def check(options, instance):
-                if options.allow_hat == False and instance.hat is not None:
-                    raise ValidationError("Hat is not allowed")
+    def test_raises_error_on_non_optional_parameter(self):
+        self.assertRaises(ValidationError, Person)
+        self.assertRaises(ValidationError, Person, age=25)
+        self.assertRaises(ValidationError, Person, name="No age provided    ")
 
-            def say_hello(self):
-                return "Hello, I'm " + self.name
 
-        # Creates an object with those fields.
-        person = Person.make(name="John")
-        self.assertEqual(Person.make(person.serialize()), person)
-        self.assertEqual(person.say_hello(), "Hello, I'm John")
-        self.assertRaises(ValidationError, Person(allow_hat=False).make, name='John', hat='Fedora')
-        person.age = 10
-        def do_validation_error():
-            person.is_dead = 12
-        self.assertRaises(ValidationError, do_validation_error)
-        person.is_dead = True
-        person.privkey = b'1234'
-        self.assertEqual(Person.make(person.serialize()), person)
-        def do_validation_error():
-            person.name = ('too long'*30)
-        self.assertRaises(ValidationError, do_validation_error)
-        person.age = 123412341234123412341234
-        self.assertEqual(Person.make(person.serialize()), person)
-        other_person = Person.make(name="Alice")
-        self.assertNotEqual(Person.make(person.serialize()), other_person)
-        person.children = [other_person]
-        self.assertEqual(Person.make(person.serialize()), person)
-        self.assertNotEqual(Person.make(person.serialize()), None)
+class TestValueChecker(unittest.TestCase):
+    def test_raises_error_on_negative_age(self):
+        self.assertRaises(ValidationError, Person, age=-1, name="John")
+    def test_raises_error_on_long_name(self):
+        self.assertRaises(ValidationError, Person, age=25, name="Way Too Long"*50)
+    def test_works_for_valid_attributes(self):
+        john = Person(age=25, name="John")
 
-    def test_big_bytes(self):
-        class SporeMessage(Field):
-            def fields():
-                method = String(max_length=100)
-                payload = Bytes()
-        big = b'\x00'*1024
-        message = SporeMessage.make(method='hello',payload=big)
-        self.assertEqual(message.payload,SporeMessage.make(message.serialize()).payload)
+"""
+    self.assertEqual(Person.make(person.serialize()), person)
+    self.assertEqual(person.say_hello(), "Hello, I'm John")
+    self.assertRaises(ValidationError, Person(allow_hat=False).make, name='John', hat='Fedora')
+    person.age = 10
+
+    def do_validation_error():
+        person.is_dead = 12
+
+    self.assertRaises(ValidationError, do_validation_error)
+    person.is_dead = True
+    person.privkey = b'1234'
+    self.assertEqual(Person.make(person.serialize()), person)
+
+    def do_validation_error():
+        person.name = ('too long' * 30)
+
+    self.assertRaises(ValidationError, do_validation_error)
+    person.age = 123412341234123412341234
+    self.assertEqual(Person.make(person.serialize()), person)
+    other_person = Person.make(name="Alice")
+    self.assertNotEqual(Person.make(person.serialize()), other_person)
+    person.children = [other_person]
+    self.assertEqual(Person.make(person.serialize()), person)
+    self.assertNotEqual(Person.make(person.serialize()), None)
+
+
+def test_big_bytes(self):
+    class SporeMessage(Field):
+        def fields():
+            method = String(max_length=100)
+            payload = Bytes()
+
+    big = b'\x00' * 1024
+    message = SporeMessage.make(method='hello', payload=big)
+    self.assertEqual(message.payload, SporeMessage.make(message.serialize()).payload)
+
+"""
 
 if __name__ == '__main__':
     unittest.main()
